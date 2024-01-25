cbf8eccf22d826a59a2f02935a9ba2886e23cc9c
Meawkung <Natdanai_Pech@hotmail.com>
2024-01-25 09:54:33 +0700

    update promise

--- UP-HTML-Course/LAB07 Mustache/ex1/index.js
+++ UP-HTML-Course/LAB07 Mustache/ex1/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const mustacheExpress = require('mustache-express');
+const { rejects } = require('assert');
+const { error } = require('console');
  
 const app = express();
 const port = 8000;
@@ -18,13 +20,42 @@ let root_path = path.resolve(__dirname, 'static');
 app.use(express.static(root_path));
  
 app.get('/txt/:cal', (req, res) => {
-    cal = req.params['cal'].split(',')
+    const operRegex = /[\+\-\*\/r]/;
+    cal = req.params['cal'].split(operRegex)
+
     a = cal[0]
-    b = cal[2]
-    operator = cal[1]
-    c = calculator(a, b, operator)
-    operator = operator == 'r' ? '/' : operator
-    res.render('simple', { a, b, operator, c })
+    b = cal[1]
+
+operator = req.params['cal'].replace(a, '').replace(b, '')
+let msg = ''
+var txt = ''
+
+const promise = new Promise((resolve,reject) => {
+    if (b == 0 && operator == 'r') {
+        msg = 'Cannot divide by zero'
+        reject(msg)
+    }
+    else {
+        c = calculator(a, b, operator)
+        operator = operator == 'r' ? '/' : operator
+        msg = `${a} ${operator} ${b} = ${c}`
+        resolve(msg)
+    }
+})
+
+    promise.then(
+        result => {
+            txt = result
+            console.log(txt)
+            res.send(txt)
+        },error => {
+            txt = 'found error'
+        })
+    promise.catch(error => {
+        txt += ', Exception: ' + error
+        console.log(txt)
+        res.send(txt)
+    })
 });
  
 calculator = (a, b, operator) => {
